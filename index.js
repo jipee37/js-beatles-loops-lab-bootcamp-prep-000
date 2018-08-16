86bfaf5aa3ce1d075560cae42b9abe2c51f9ee77
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-16 08:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   const newFacts = []
-  for(let i=0; i< facts.length; ++i){
+  let i = 0;
+  while(i< facts.length){
     newFacts.push(facts[i] + " !!!")
+    ++i
   }
   return newFacts
+}
+
+function iLoveTheBeatles(num){
+  const empty = []
+  do {
+    empty.push("I love the Beatles!")
+    num++    
+  } while(num< 15)
+  return empty
 }
